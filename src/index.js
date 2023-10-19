27b30010e5a1e4e39c961249927e4f8894490774
Bernardo Araujo <bernardoaraujons@gmail.com>
2023-10-18 21:52:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE");
-  res.header("Access-Control-Allow-hEADERS", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   app.use(cors());
   next();  
 })
