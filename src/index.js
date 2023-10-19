ed4e13b589ce9ee73642bccc9f8f39fe0eda6d2b
Bernardo Araujo <bernardoaraujons@gmail.com>
2023-10-18 22:41:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ const port = 3000
 const{ Categories } = require("./Categories")
 const{ Projects } = require("./Projects")
 
-app.use(cors());
+//app.use(cors());
+/*app.use(cors());
 app.use(bodyParser.json())
-
+*/
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   app.use(cors());
   next();  
 })
